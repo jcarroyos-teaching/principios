e30d60689781efb1af02c49752185a610ff3d676
jarroyoskg <jarroyo@skgtecnologia.com>
2024-03-16 13:53:12 -0500

    explicacion de clases

--- principios.js
+++ principios.js
@@ -1,13 +1,41 @@
+class Persona {
+    constructor(nombre, apellido, edad, fechaNacimiento, asisitio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.asisitio = asisitio;
+    }
 
-let persona = {
-    nombre : "Juan Carlos",
-    apellido : "Arroyo",
-    edad : 43,
-    fechaNacimiento : "1980-09-10",
-    asisitio : true,
-    saludar : function() {
+    salude() {
         console.log(`Hola ${this.nombre}, tengo ${this.edad} años`);
     }
 }
 
-persona.saludar();
+class Alumno extends Persona {
+    constructor(nombre, apellido, edad, fechaNacimiento, asisitio, grado) {
+        super(nombre, apellido, edad, fechaNacimiento, asisitio);
+        this.grado = grado;
+    }
+    micurso() {
+        console.log(`Estoy en el grado ${this.grado}`);
+    }
+}
+
+class Profesor extends Persona {
+    constructor(nombre, apellido, edad, fechaNacimiento, asisitio, asignatura) {
+        super(nombre, apellido, edad, fechaNacimiento, asisitio);
+        this.asignatura = asignatura;
+    }
+    yodicto() {
+        console.log(`Dicto la asignatura de ${this.asignatura}`);
+    }
+}
+
+let juan = new Alumno("Juan Carlos", "Arroyo", 43, "1980-09-10", true, "10");
+juan.salude();
+juan.micurso();
+
+let maria = new Profesor("Maria", "Perez", 33, "1988-09-10", false, "Matemáticas");
+maria.salude();
+maria.yodicto();
